1ea5c22a75f06cf9d043a7029ef37253f47ae4d4
666borz666 <iloveass3333@gmail.com>
2023-08-24 18:56:13 -0600

    Programa funcional

---
+++ DF 3.16 No F.py
@@ -0,0 +1,16 @@
+#Creado por: Daniel Campos
+#Fecha de cración: 24-08-2023 6:40pm
+#última modificación: 24-08-2023
+#Versión 3.10.6
+
+serie=0
+n=int(input("Digite el número de términos de la serie: "))
+i=1
+if n>0:
+    while i<n:
+        serie+=i**i
+        i+=1
+    else:
+        print(serie)
+else: 
+    print("Digite una cantidad mayor que 0")
