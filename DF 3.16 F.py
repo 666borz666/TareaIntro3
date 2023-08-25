9c42308a7b89902df35637498c4d2c937cf61db1
666borz666 <iloveass3333@gmail.com>
2023-08-24 19:36:31 -0600

    Explicaciones de las funciones corregidas (salidas)

---
+++ DF 3.16 F.py
@@ -0,0 +1,15 @@
+#Creado por: Daniel Campos
+#Fecha de cración: 24-08-2023 7:30pm
+#última modificación: 24-08-2023 7: pm
+#Versión 3.10.6
+
+#def funciones
+def resSer(pserie,i):
+    """Función: calcula la serie de n números la cual es sumando de manera: n**n
+    Entradas:
+        pserie: acumulador de los resultados
+        i: control del ciclo 
+    Salidas: 
+        pserie: todos los valores ya acumulados y sumados
+    """
+    
